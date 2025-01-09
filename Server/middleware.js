f769f4524c19a711e3612003a5f8abc1e7e94b98
Alok Rai <101451098+AlokRai123@users.noreply.github.com>
2025-01-09 22:21:15 +0530

    middleware and db

---
+++ Server/middleware.js
@@ -0,0 +1,92 @@
+const express = require("express");
+const users = require('../01Project/MOCK_DATA.json');
+const fs = require('fs');
+const mongoose = require('mongoose');
+const { type } = require("os");
+const PORT = 7000;
+
+const app = express();
+
+
+// this is a our middleware-----
+app.use(express.urlencoded({extended : false}));
+
+app.use((req,res,next) =>{
+    console.log("hello from middleware 1");
+    next();
+})
+app.use((req,res,next) => {
+   fs.appendFile("text.txt",`${Date.now()} :${req.method}  ${req.path}\n`,
+   (err,data) =>
+    {next();
+
+   })
+})
+
+
+
+const userSchema = new mongoose.Schema({
+    firstName : {
+        type : String,
+        required : true
+    },
+    lastName : {
+        type : String,
+    },
+    email : {
+        type : String,
+        required : true,
+        unique : true
+    },
+    Job_title :{
+        type : String,
+    },
+
+    Gender : {
+        type : String,
+    }
+})
+
+const user = mongoose.model("user" , userSchema);
+
+// mongoose.connect('mongodb://127.0.0.1:27017/youtube-app-1')
+// .then(() => console.log("MongoDb Connected"))
+// .catch((err) => console.log("MongoDb Error" ,err));
+
+app.get('/', (req,res) =>{
+    return res.end("this is a home page of server")
+})
+
+app.get("/api/users" , (req,res) => {
+    return res.json(users);
+})
+
+
+
+app.get("/api/users/:id" , (req,res) => {
+    const id = Number(req.params.id);
+    const user = users.find((user) => user.id === id);
+    return res.json(user);
+})
+
+app.post("/api/users",async (req,res) =>{
+
+
+
+     if(!user || !firstName || !email || ! Gender){
+        return res.status(400).json({msg : "All field are required"});
+     }
+
+     const result = await user.create({
+          firstName :body.firstName,
+          lastName : body.lastName,
+          email : body.email,
+          gender : body.gender,
+          Job_title : body.Job_title,
+     })
+     return res.status(201).json({msg : "success"});
+})
+
+app.listen(PORT , () =>{
+    console.log('server is create')
+})
