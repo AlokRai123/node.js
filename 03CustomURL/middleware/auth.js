815021ee6612455d0a0ee7948231cd0c87430843
Alok Rai <101451098+AlokRai123@users.noreply.github.com>
2025-01-20 23:23:14 +0530

    autherization

--- 03CustomURL/middleware/auth.js
+++ 03CustomURL/middleware/auth.js
@@ -1,15 +1,43 @@
 const {getUser} = require('../service/auth')
 
-async function restrictToLoggedinUserOnly(req,res,next){
-const userUid = req.cookieS?.uid;
+function checkForAuthentication(req,res,next){
+    const authorizationHeadervalue = req.headers["authorization"];
+    req.user = null;
 
-if(!userUid) return res.redirect("login");
-const user = getUser(userUid);
+    if(!authorizationHeadervalue || !authorizationHeadervalue.startsWith("Bearer"))
+        return next();
 
-if(!user) return res.redirect("/login");
+    const token = authorizationHeadervalue.split("Bearer")[1];
+    const user = getUser(token);
 
-req.user = user;
-next();
+    req.user = user;
+    return next();
 }
 
-module.exports = {restrictToLoggedinUserOnly};
+function restrictTo(roles){
+    return function (req,res,next){
+        if(!req.user) return res.redirect("/login");
+
+        if(!roles.includes(req.user.role)) return res.end("UnAuthorized");
+
+        return next();
+    }
+}
+
+// async function restrictToLoggedinUserOnly(req,res,next){
+    //     const userUid = req.headers["Authorization"];
+    //     console.log(req.headers);
+    // // const userUid = req.cookieS?.uid;
+    
+    //  if(!userUid) return res.redirect("login");
+    //  const token = userUid.split("Bearer")[1];
+    //  const user= getUser(token);
+    // // const user = getUser(userUid);
+    
+    // if(!user) return res.redirect("/login");
+    
+    // req.user = user;
+    // next();
+    // }
+    
+    module.exports = {checkForAuthentication,restrictTo};
